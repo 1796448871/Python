6d086c965e237be73fe8d570c911c11e550952f9
DanDing <1796448871@qq.com>
2025-03-20 10:35:46 +0800

    Tab缩进，忽略哪些

---
+++ test/test.py
@@ -0,0 +1,11 @@
+fruits = ['apple', 'banana', 'orange']
+
+for index, fruit in enumerate(fruits):
+    print(index, fruit)
+# 运行结果为：
+# Copy code
+# 0 apple
+# 1 banana
+# 2 orange
+a=enumerate(fruits) #可以自己想想是什么
+print(a)
