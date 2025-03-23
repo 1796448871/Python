7dd6337cda54554be53a4d2d030f1b580547a2ac
DanDing <1796448871@qq.com>
2025-03-23 23:46:18 +0800

    1

--- 刷题/A+B的和读入数据方式.py
+++ 刷题/A+B的和读入数据方式.py
@@ -1,14 +1,15 @@
 import sys
 
 try:
-    while True:  # 持续读取输入，直到没有更多输入
+    while True:  #python只认识True,不认识true
         # 读取第一行，获取N
         N = int(input().strip())
-        
-        # 读取接下来的N行，每行包含两个整数a和b
         for _ in range(N):
-            line = input().strip()
-            a, b = map(int, line.split())  # 将字符串分割并转换为整数
+            line = input().strip()#strip去前后空格
+            a, b = map(int, line.split())  #split分割
             print(a + b)  # 输出a和b的和
 except EOFError:
     pass  # 当没有更多输入时，捕获EOFError并结束程序
+
+
+# 在try里面只能break  return 语句不能在全局作用域中使用
